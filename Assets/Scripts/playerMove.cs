24528c235dbac0006bd06674e0136f7643e5074f
ymorts <minamicute@gmail.com>
2026-05-09 10:14:45 -0400

    initial commit

--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -22,7 +22,7 @@ public class playerMove : MonoBehaviour
     void Update()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheckBottom.position, 0.1f, groundLayer) || Physics2D.OverlapCircle(groundCheckTop.position, 0.1f, groundLayer);
-       transform.Translate(new Vector2(x:speed * Time.deltaTime, y:0));
+        transform.Translate(new Vector2(x:speed * Time.deltaTime, y:0));
         jump();
     }
 
