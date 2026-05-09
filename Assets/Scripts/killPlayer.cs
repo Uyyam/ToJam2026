46c180877800dd11f4b6113c3a7e895fcd219a25
Evan Inrig <inrig003@gmail.com>
2026-05-08 21:39:53 -0700

    Respawn poijnt for player/kill zones

---
+++ Assets/Scripts/killPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject scrubber;
+    public Transform respawnPoint;
+
+    //current distance between scrubber and player, respawns relative to player position
+    public const float scrubberOffset = -6f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            // TODO: have music rewind to position - make camera jump(?) to respawn point
+            player.transform.position = respawnPoint.position;
+            scrubber.transform.position = respawnPoint.position + new Vector3(scrubberOffset, 0, 0);
+        }
+    }
+}
